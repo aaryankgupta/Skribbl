2f495096f2a99290ea34f4bfa985673094980c83
sankalppar <sankalpparasharblog@gmail.com>
2021-10-10 02:32:29 +0530

    Lobby feature integrated

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This repository contains our course project `Skribbl Skribbl` of `CS251` course `Software Systems Laboratory`  done under prof. `Amitabh Sanyal` in our third semester at IITB.
+This repository contains our course project `Skribbl Skribbl` of `CS251` course `Software Systems Laboratory`  done under prof. `Amitabha Sanyal` in our third semester at IITB.
---
+++ public/script.js
@@ -0,0 +1,47 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+const room_container = document.getElementById('room-container')
+
+if (messageForm != null){
+    const name = prompt('What is your name?');
+    appendMessage('You joined');
+    socket.emit('new-user', roomName, name);
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const message = messageInput.value
+        appendMessage(`You: ${  message}`)
+        socket.emit('send-chat-message', roomName, message)
+        messageInput.value = ''
+    });
+}
+
+
+
+socket.on('chat-message',data => {
+    appendMessage(`${data.name}: ${data.message}`)
+});
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+});
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+});
+
+socket.on('room-created', room => {
+    const roomElement = document.createElement('div');
+    roomElement.innerText = room;
+    const roomLink = document.createElement('a');
+    roomLink.href = `/${room}`;
+    roomLink.innerText = 'join';
+    room_container.append(roomElement);
+    room_container.append(roomLink);
+})
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
